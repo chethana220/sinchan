7e898e45cfd67ad3ce86b01e69da1dbe0ab9755f
chethana220 <chethna965@gmail.com>
2024-11-21 11:36:45 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,19 @@
+import turtle as t
+import time
+
+t.color ("blue")
+t .begin_fill()
+
+ counter=0
+ 
+        while counter < 4:
+     tforward(100)
+     t.lft(90)
+     counter = counter+1
+
+t end_fill()
+time.slee(5)
+
+
+
+
